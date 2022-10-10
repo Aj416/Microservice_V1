3ff062e2b12b979c539df796c036d1777591fc98
Ajay Abraham <ajay.ab@hotmail.com>
2022-10-10 15:03:09 +0530

    create initial draft for catalog api project

---
+++ src/Services/Category/Category.API/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Category.API.Entities
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+        public string Namw { get; set; }
+        public string Description { get; set; }
+    }
+}
